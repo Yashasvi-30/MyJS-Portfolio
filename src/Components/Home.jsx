7898a402f178a64d6042601663dd3cdac9ee3054
Yashasvi-30 <yashasvi30gupta@gmail.com>
2024-09-03 18:55:00 +0530

    hello

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -28,7 +28,7 @@ const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
       <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem", color: "white" }}>
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
